a6ae91d39023239f1155c018b9ea4f5c492713a9
EllieWalsch <elliewalsch4@gmail.com>
2023-01-03 20:42:59 -0600

    Add classwork

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import config from "./config.js";
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection(config.db);
+
+const [employees] = await connection.execute(
+  "SELECT * from employees LIMIT 10"
+);
+console.log(employees);
+
+connection.end();
